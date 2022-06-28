422735d759e1282621c48268c989ac02cdec03c9
LeoXiaoNian <50272068+LeoXiaoNian@users.noreply.github.com>
2022-06-28 23:07:55 +0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AMesh3D'
+copyright = '2022, AMesh3D'
+author = 'Leo Xiao'
 
 release = '0.1'
 version = '0.1.0'
